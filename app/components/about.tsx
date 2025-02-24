d1b2a27f1558bea4f84250cd3b919a47ae4df2e3
Rwitesh Bera <rwiteshbera@gmail.com>
2025-02-24 22:21:45 +0530

    Update about.tsx

--- app/components/about.tsx
+++ app/components/about.tsx
@@ -7,11 +7,7 @@ export default function About() {
       <p>
         I am a Software Engineer based in India. Currently, I work at{" "}
         <Link to="https://www.kloudmate.com/" name="KloudMate" className="underline" />
-        . I enjoy building programs that solve real problems. I like designing
-        applications, understanding how things work under the hood, and making
-        systems reliable. With a Bachelor degree in Computer Science and
-        Engineering, I enjoy experimenting, improving my approaches, and finding
-        smarter ways to build software.
+        .  I enjoy designing applications, understanding systems at a deeper level, and making them more reliable. With a Bachelor's degree in Computer Science and Engineering, I'm always experimenting, refining my approaches, and finding smarter ways to build software.
       </p>
     </Section>
   );
